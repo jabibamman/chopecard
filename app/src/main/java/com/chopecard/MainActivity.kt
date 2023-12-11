e1a0db0dceed43a0047a162980848ba543fc68ab
James ABIB <james.bidule@gmail.com>
2023-12-11 16:55:37 +0100

    feat(footer): add dark mode footer

--- app/src/main/java/com/chopecard/MainActivity.kt
+++ app/src/main/java/com/chopecard/MainActivity.kt
@@ -2,12 +2,15 @@ package com.chopecard
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatDelegate
 import injectModuleDependencies
 import parseAndInjectConfiguration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
         setContentView(R.layout.activity_main)
         parseAndInjectConfiguration()
         injectModuleDependencies(this)
