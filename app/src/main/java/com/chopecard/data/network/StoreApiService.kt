10795139717ff44722a393521dbe30b1bdae4609
Charles <charlescretois@gmail.com>
2024-01-18 09:18:51 +0100

    fix: refactor store endpoints

--- app/src/main/java/com/chopecard/data/network/StoreApiService.kt
+++ app/src/main/java/com/chopecard/data/network/StoreApiService.kt
@@ -19,9 +19,6 @@ import retrofit2.http.*
 
 interface StoreApiService {
 
-    @POST("/v1/user")
-    fun createUser(@Body userDTO: UserDTO): Call<String>
-
     @GET("/v1/stores")
     fun getStores(): Call<List<Store>>
 
@@ -37,38 +34,11 @@ interface StoreApiService {
     @POST("/v1/stores/{storeId}/products/{productId}")
     fun createProduct(@Path("storeId") storeId: Int, @Path("productId") productId: Int, @Body productStoreDTO: ProductStoreDTO): Call<String>
 
-    @POST("/v1/admin/tickets/create")
-    fun createTicket(@Body ticket: Ticket): Call<Long>
-
-    @POST("/v1/admin/products/create")
-    fun createProduct_1(@Body product: Product): Call<Long>
-
-    @DELETE("/v1/stores/{storeId}/products/{productsId}")
-    fun deleteProduct(@Path("storeId") storeId: Long, @Path("productsId") productsId: Long): Call<String>
-
-    @PATCH("/v1/stores/{storeId}/products/{productsId}")
-    fun updateProduct(@Path("storeId") storeId: Long, @Path("productsId") productsId: Long, @Body patchProductDTO: PatchProductDTO): Call<ProductStore>
-
-    @GET("/v1/user/{userId}")
-    fun getUserById(@Path("userId") userId: Long): Call<Users>
-
-    @DELETE("/v1/user/{userId}")
-    fun deleteUser(@Path("userId") userId: Long): Call<String>
-
-    @GET("/v1/user/{userId}/reserves")
-    fun getReservations(@Path("userId") userId: Long): Call<List<UserReservation>>
-
-    @GET("/v1/user/{userId}/reserves/{reserveId}")
-    fun getReservation(@Path("userId") userId: Long, @Path("reserveId") reserveId: Long): Call<UserReservation>
-
     @GET("/v1/stores/{storeId}")
     fun getStoresById(@Path("storeId") storeId: Int): Call<Store>
 
     @DELETE("/v1/stores/{storeId}")
     fun deleteStore(@Path("storeId") storeId: Int): Call<String>
-
-    @GET("/v1/stores/{storeId}/user/{userId}/reservers")
-    fun getReservers(@Path("storeId") storeId: Long, @Path("userId") userId: Long): Call<List<UserReservation>>
 
     @GET("/v1/stores/{storeId}/reserves")
     fun getReserves(@Path("storeId") storeId: Int): Call<List<UserReservation>>
@@ -76,42 +46,9 @@ interface StoreApiService {
     @GET("/v1/stores/{storeId}/products")
     fun getStoreProducts(@Path("storeId") storeId: Int): Call<List<ProductStore>>
 
-    @GET("/v1/products")
-    fun getProducts_1(): Call<List<Product>>
-
-    @GET("/v1/products/{productId}")
-    fun getProduct_1(@Path("productId") productId: Long): Call<Product>
-
-    @GET("/v1/displays")
-    fun getDisplays(): Call<List<Display>>
-
     @GET("/v1/displays/{storeId}")
     fun getDisplay(@Path("storeId") storeId: Int): Call<Display>
 
-    @GET("/v1/cards")
-    fun getCards(): Call<List<Card>>
-
-    @GET("/v1/cards/{productId}")
-    fun getCard(@Path("productId") productId: Long): Call<Card>
-
-    @GET("/v1/boosters")
-    fun getBoosters(): Call<List<Booster>>
-
-    @GET("/v1/boosters/{productId}")
-    fun getBoosters_1(@Path("productId") productId: Long): Call<Booster>
-
-    @GET("/v1/admin/tickets")
-    fun getAllTickets(): Call<List<Ticket>>
-
-    @GET("/v1/admin/tickets/{ticketId}")
-    fun getTicketById(@Path("ticketId") ticketId: Long): Call<Ticket>
-
-    @DELETE("/v1/admin/tickets/{ticketId}")
-    fun deleteTicket(@Path("ticketId") ticketId: Long): Call<Void>
-
     @DELETE("/v1/stores/{storeId}/user/{userId}/reservation/{reserveId}/unreserve")
     fun unreserveProduct(@Path("storeId") storeId: Int, @Path("userId") userId: Int, @Path("reserveId") reserveId: Int): Call<String>
-
-    @DELETE("/v1/admin/products/{productId}")
-    fun deleteProduct_1(@Path("productId") productId: Long): Call<Void>
 }
