b7f74fc8e2525111b2ae2079e71a09ec672d4f23
Ronan Kielt <rkielt@myges.fr>
2024-02-05 12:55:28 +0100

    feat: add loading for admin layout when getting tickets from api

--- app/src/main/java/com/chopecard/presentation/viewModel/TicketViewModel.kt
+++ app/src/main/java/com/chopecard/presentation/viewModel/TicketViewModel.kt
@@ -8,17 +8,29 @@ import com.chopecard.domain.models.Ticket
 import com.chopecard.domain.usecases.GetTicketsUseCase
 import kotlinx.coroutines.launch
 
-class TicketViewModel(private val getTicketsUseCase: GetTicketsUseCase): ViewModel() {
-    val ticketsLiveData = MutableLiveData<List<Ticket>>()
+/** Represents the UI state. */
+sealed class TicketDataState {
+    object Loading : TicketDataState()
+    data class Success(val tickets: List<Ticket>): TicketDataState()
+    data class Error(val exception: Throwable): TicketDataState()
+}
+
+class TicketViewModel(
+    private val getTicketsUseCase: GetTicketsUseCase
+): ViewModel() {
+
+    val ticketsLiveData = MutableLiveData<TicketDataState>()
 
     fun getTickets() {
+        ticketsLiveData.postValue(TicketDataState.Loading)
         viewModelScope.launch {
             try {
                 val tickets = getTicketsUseCase.execute()
-                ticketsLiveData.postValue(tickets)
+                ticketsLiveData.postValue(TicketDataState.Success(tickets))
+                Log.d("TicketViewModel", "Tickets: $tickets")
             } catch (e: Exception) {
                 Log.e("TicketViewModel", "Exception when calling use case: ", e)
-                ticketsLiveData.postValue(emptyList())
+                ticketsLiveData.postValue(TicketDataState.Error(e))
             }
         }
     }
--- app/src/main/java/com/chopecard/ui/activity/AdminActivity.kt
+++ app/src/main/java/com/chopecard/ui/activity/AdminActivity.kt
@@ -1,10 +1,13 @@
 package com.chopecard.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chopecard.R
+import com.chopecard.presentation.viewModel.TicketDataState
 import com.chopecard.presentation.viewModel.TicketViewModel
 import com.chopecard.ui.adapter.TicketListAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -23,18 +26,27 @@ class AdminActivity : BaseActivity() {
 
     private fun setupRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.rvTickets)
-        val adapter = TicketListAdapter(mutableListOf()) { ticket ->
-            val intent = Intent(this, AdminActivity::class.java)
-            startActivity(intent)
-        }
+        val adapter = TicketListAdapter(mutableListOf()) { }
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
     }
 
     private fun observeTickets() {
-        ticketViewModel.ticketsLiveData.observe(this) { tickets ->
-            val adapter = findViewById<RecyclerView>(R.id.rvTickets).adapter as? TicketListAdapter
-            adapter?.updateList(tickets)
+        ticketViewModel.ticketsLiveData.observe(this) { ticketDataState ->
+            when(ticketDataState) {
+                is TicketDataState.Loading -> {
+                    findViewById<ProgressBar>(R.id.adminProgressBar).visibility = View.VISIBLE
+                }
+                is TicketDataState.Success -> {
+                    findViewById<ProgressBar>(R.id.adminProgressBar).visibility = View.GONE
+                    val adapter = findViewById<RecyclerView>(R.id.rvTickets).adapter as? TicketListAdapter
+                    adapter?.updateList(ticketDataState.tickets)
+                }
+                is TicketDataState.Error -> {
+                    findViewById<ProgressBar>(R.id.adminProgressBar).visibility = View.GONE
+                    Log.e("AdminActivity", "Error getting tickets", ticketDataState.exception)
+                }
+            }
         }
     }
 }
