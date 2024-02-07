0195bd29c7d2e6df4c4bbad0c02b526c4695f133
Ronan Kielt <rkielt@myges.fr>
2024-02-07 19:27:31 +0100

    feat: logout button logout the current connected user

--- app/src/main/java/com/chopecard/ui/activity/ShopListActivity.kt
+++ app/src/main/java/com/chopecard/ui/activity/ShopListActivity.kt
@@ -1,16 +1,19 @@
 package com.chopecard.ui.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import androidx.fragment.app.FragmentContainerView
 import com.chopecard.R
+import com.chopecard.data.storage.UserPreferences
 import com.chopecard.presentation.fragment.ShopListFragment
 
 class ShopListActivity : BaseActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_list)
         setupFooter()
+        setupListener()
         if (savedInstanceState == null) {
             val fragmentContainer = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
                 ?: throw IllegalStateException("Fragment container not found in the layout")
@@ -21,4 +24,11 @@ class ShopListActivity : BaseActivity() {
         }
     }
 
+    private fun setupListener() {
+        findViewById<ImageButton>(R.id.btnLogout)?.setOnClickListener {
+            Log.d("ShopListActivity", "Logout button clicked")
+            UserPreferences.clearAllPreference(this)
+            setContentView(R.layout.activity_login_register)
+        }
+    }
 }
