b3f21a9938ca19f7e46169f2b31263e7ab775cc3
Ronan Kielt <rkielt@myges.fr>
2024-03-03 17:44:09 +0100

    chore: use newer versions of package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,22 +67,22 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     // Compose Material
-    implementation("androidx.compose.material:material:1.4.1")
-    implementation("com.google.android.material:material:1.7.0-rc01")
+    implementation("androidx.compose.material:material:1.6.2")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.material3:material3-android:1.2.0")
 
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
-    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
 
     // Koin pour Kotlin
@@ -94,20 +94,20 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Hhttp logging
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
-    implementation("androidx.databinding:databinding-runtime:8.2.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation("androidx.databinding:databinding-runtime:8.3.0")
 
     // Glide
-    implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.fragment:fragment:1.6.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
